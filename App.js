19bb0045d28f6f40f4367c5aadfa0e85ccce74c4
Yossi <yosef.d.cohen@gmail.com>
2020-08-03 21:20:21 -0500

    initial commit, add code for basic checkbox using react

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import CheckBox from "./components/CheckBox"
+
+function App() {
+    return (
+        <div>
+            <CheckBox />
+        </div>
+    )
+}
+
+export default App
---
+++ components/CheckBox.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function CheckBox() {
+    return (
+        <div>
+            <div>
+                <input type="checkbox"/>
+                <label>Carrots</label>
+            </div>
+
+            <div>
+                <input type="checkbox"/>
+                <label>Apples</label>
+            </div>
+        </div>
+    )
+}
+
+export default CheckBox
